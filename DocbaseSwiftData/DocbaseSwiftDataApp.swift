6cbe23bb38ad91971e354cea0409f5da261b23eb
superkookai <weerawut@live.com>
2025-04-06 10:30:29 +0700

    Initial Commit

---
+++ DocbaseSwiftData/DocbaseSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocbaseSwiftDataApp.swift
+//  DocbaseSwiftData
+//
+//  Created by Weerawut Chaiyasomboon on 06/04/2568.
+//
+
+import SwiftUI
+
+@main
+struct DocbaseSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
